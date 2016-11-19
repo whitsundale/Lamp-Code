7c9c6a595a5d085d050dcefab95e92eaaf922f3b
whitsundale <amreghitto21@thompsonschools.org>
2016-11-18 20:50:49 -0700

    Update Custom.js

--- Custom.js
+++ Custom.js
@@ -1,4 +1,4 @@
-var count = 1;
+var count = 2;
 
 function increment() {
   document.getElementById('custom').innerHTML += "\n<input type=\"number\" value=\"" + count.toString() + "\" id=\"value" + count.toString() + "\">\n<input type=\"color\">";
@@ -20,8 +20,38 @@ function decrement() {
 }
 
 function subm() {
-  var webp = "https://agent.electricimp.com/LbCwF8_YvW3f"
-  $.get(webp, function(data, status){
-    if (status != "success")
-      alert("Send failed. Please try again. Message:" + data);
-  });
+  var form = $("#custom").serializeArray();
+  var red = null;
+  var green = null;
+  var blue = null;
+  var index = null;
+  for (i = 0; i < form.length; i++) {
+    switch (form[i]["name"]){
+      case "red":
+        red = form[i]["value"];
+        break;
+       case "green":
+        green = form[i]["value"];
+        break;
+      case "blue":
+        blue = form[i]["value"];
+        break;
+      case "index":
+        index = form[i]["value"];
+        break;
+      default:
+        alert("Invalid input!");
+     }
+    if (red && green && blue && index) {
+      var webp = "https://agent.electricimp.com/LbCwF8_YvW3f?red=" + red + "&green=" + green + "&blue=" + blue + "&index=" + index;
+      $.get(webp, function(data, status){
+        if (status != "success")
+          alert("Send failed. Please try again. Message:" + data);
+      });
+      red = null;
+      green = null;
+      blue = null;
+      index = null;
+    }
+  }
+}
